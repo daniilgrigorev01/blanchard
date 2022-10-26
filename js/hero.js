fd9b548ef56e4a360c0742a01ba68b38b6c38145
Daniil Grigorev <daniilgrigorev01@mail.ru>
2022-10-26 21:42:10 +0500

    added section hero up to 320px

---
+++ js/hero.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  allowTouchMove: false,
+  speed: 5000,
+  autoplay: {
+    delay: 10000,
+  },
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+});
