5ba134c7a2411399f06133cbe0cdea7453a25c96
Daniil Grigorev <daniilgrigorev01@mail.ru>
2022-11-18 00:02:58 +0500

    added section catalog up to 320px

--- js/catalog.js
+++ js/catalog.js
@@ -1,5 +1,88 @@
-new Accordion(".catalog__accordion", {
-  elementClass: "catalog-accordion__item",
-  triggerClass: "catalog-accordion__btn",
-  panelClass: "catalog-accordion__panel",
+const accordionCatalog = () => {
+  new Accordion(".catalog__accordion", {
+    elementClass: "catalog-accordion__item",
+    triggerClass: "catalog-accordion__btn",
+    panelClass: "catalog-accordion__panel",
+  });
+};
+
+function aboutArtist(params) {
+  const link = document.querySelectorAll(`.${params.linkClass}`);
+  const artist = document.querySelectorAll(`.${params.artistClass}`);
+
+  link.forEach((current) => {
+    current.addEventListener("click", function () {
+      let linkPath = current.dataset.path;
+
+      artist.forEach((el) => {
+        let artistTarget = el.dataset.target;
+
+        if (linkPath === artistTarget) {
+          el.classList.add(params.activeClass);
+        } else {
+          el.classList.remove(params.activeClass);
+        }
+      });
+    });
+  });
+}
+
+function closeAccordion(params) {
+  const link = document.querySelectorAll(`.${params.linkClass}`);
+  const accordionItem = document.querySelectorAll(
+    `.${params.accordionItemClass}`
+  );
+  const accordionPanel = document.querySelectorAll(
+    `.${params.accordionPanelClass}`
+  );
+
+  link.forEach((current) => {
+    current.addEventListener("click", function () {
+      accordionItem.forEach((el) => {
+        el.classList.remove(`${params.activeClass}`);
+      });
+      accordionPanel.forEach((el) => {
+        el.style.height = 0;
+      });
+    });
+  });
+}
+
+function closeAccordionEmpty(params) {
+  const link = document.querySelectorAll(`.${params.linkClass}`);
+  const accordionItem = document.querySelectorAll(`.${params.accordionItemClass}`);
+  const accordionPanel = document.querySelectorAll(`.${params.accordionPanelClass}`);
+
+  link.forEach((current) => {
+    current.addEventListener("click", function () {
+      accordionItem.forEach((el) => {
+        el.classList.remove(`${params.activeClass}`);
+      });
+      accordionPanel.forEach((el) => {
+        el.style.height = 0;
+      });
+    });
+  });
+}
+
+accordionCatalog();
+
+aboutArtist({
+  linkClass: "catalog-accordion__artist",
+  artistClass: "catalog__artist",
+  activeClass: "is-active",
+});
+
+closeAccordion({
+  linkClass: "catalog-accordion__artist",
+  accordionItemClass: "catalog-accordion__item",
+  accordionPanelClass: "catalog-accordion__panel",
+  activeClass: "is-active",
+});
+
+closeAccordionEmpty({
+  linkClass: "catalog-accordion-empty__link",
+  accordionItemClass: "catalog-accordion__item",
+  accordionPanelClass: "catalog-accordion__panel",
+  activeClass: "is-active",
 });
